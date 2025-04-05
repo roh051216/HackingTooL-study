7a11a6545c5e4f43a9bb197e23b882166a7225f6
Dragun2th <46935497+roh051216@users.noreply.github.com>
2025-04-06 00:53:33 +0900

    Add files via upload

--- 20250327/src/ScoreProcessor.java
+++ 20250327/src/ScoreProcessor.java
@@ -8,38 +8,47 @@ public class ScoreProcessor {
 
         int[] scores = new int[4];
 
-        System.out.println("각 과목의 점수를 입력하세요.");
+        System.out.println("과목의 점수를 입력하세요.");
         for (int i = 0; i < subjects.length; i++) {
             System.out.print(subjects[i] + " 점수: ");
             scores[i] = scanner.nextInt();
         }
 
         int total = 0;
-
         for (int score : scores) {
             total += score;
         }
 
+        int integerN = total / subjects.length;
 
-        int integerAverage = total / subjects.length;
+        double realN = total / (double) subjects.length;
 
-
-        double realAverage = total / (double) subjects.length;
-
-
-        System.out.println("\n------ 성적 처리 결과 ------");
+        System.out.println("\n------ 형변환 없는 평균 계산 ------");
         System.out.println("총점: " + total);
-        System.out.println("정수 평균: " + integerAverage);
-        System.out.println("실수 평균: " + realAverage);
+        System.out.println("정수 평균: " + integerN);
 
-        if (integerAverage == realAverage) {
+        System.out.println(("\n-----형변환 있는 평균 계산--------"));
+        System.out.println("실수 평균: " + realN);
+        System.out.printf("소수점 둘째자리 : %.2f\n", realN); //Gpt 사용 후 알아냄. System.out.printf 는 formatted print 를 지원
+
+
+        System.out.println("\n--------자료형 논리 비교----------");
+        if (integerN == realN) {
             System.out.println("정수 평균과 실수 평균은 동일합니다.");
-        } else if (integerAverage > realAverage) {
+        } else if (integerN > realN) {
             System.out.println("정수 평균이 실수 평균보다 큽니다.");
         } else {
             System.out.println("실수 평균이 정수 평균보다 큽니다.");
         }
 
+
+        if (realN >= 80) {
+            System.out.println("평균 80점 이상 합격 여부: 합격입니다.");
+        } else {
+            System.out.println("평균 80점 이상 합격 여부: 불합격입니다.");
+        }
+
         scanner.close();
     }
 }
+
